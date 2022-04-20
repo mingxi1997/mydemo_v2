40b290a1fbde3a3d253466d90e538297246cd0b5
mingxi1997 <49010808+mingxi1997@users.noreply.github.com>
2022-04-20 14:27:19 +0800

    Update arc_face_mnist.py

--- arc_face_mnist.py
+++ arc_face_mnist.py
@@ -5,18 +5,18 @@ import torch.nn.functional as F
 import torchvision 
 import torch.utils.data as data
 import matplotlib.pyplot as plt
-import torch.optim.lr_scheduler as lr_scheduler
-import os
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import numpy as np
+import  math
+no_plus=False
+print('no_plus :{}'.format(no_plus))
+
+
+theta_clip=False
+
+print('theta_clip :{}'.format(theta_clip))
+
+theta_clip_and_focalloss=False
+print('theta_clip_and_focalloss :{}'.format(theta_clip_and_focalloss))
 
 class FocalLoss(nn.Module):
     '''Multi-class Focal loss implementation'''
@@ -39,7 +39,7 @@ class FocalLoss(nn.Module):
     
     
 class ArcLoss(nn.Module):
-    def __init__(self,class_num,feature_num,s=30,m=0.5):
+    def __init__(self,feature_num,class_num,s=30,m=0.5):
         super().__init__()
         self.class_num=class_num
         self.feature_num=feature_num
@@ -51,21 +51,15 @@ class ArcLoss(nn.Module):
         feature = F.normalize(feature,dim=1) #128*2
         w = F.normalize(self.w,dim=0) #2*10
         cos_theat =  torch.matmul(feature,w)/self.s
- 
         # sin_theat =  torch.sqrt(1.0- torch.pow(cos_theat,2))
-        # cos_theat_m = cos_theat* torch.cos(self.m) - sin_theat* torch.sin(self.m)
-        
-        cos_theat_m = torch.cos(torch.acos(cos_theat)+self.m)
-        
+        # cos_theat_m = cos_theat* torch.cos(self.m) - sin_theat* torch.sin(self.m)    
+        cos_theat_m = torch.cos(torch.acos(cos_theat)+self.m)        
         cos_theat_ =  torch.exp(cos_theat * self.s)
-        sum_cos_theat = torch.sum( torch.exp(cos_theat*self.s),dim=1,keepdim=True)-cos_theat_
-        
-        
+        sum_cos_theat = torch.sum( torch.exp(cos_theat*self.s),dim=1,keepdim=True)-cos_theat_       
         top =  torch.exp(cos_theat_m*self.s)
         divide = (top/(top+sum_cos_theat))
 
         return divide
-import  math
 class ArcMarginProduct(nn.Module):
     r"""Implement of large margin arc distance: :
         Args:
@@ -94,8 +88,14 @@ class ArcMarginProduct(nn.Module):
     def forward(self, input, label):
         # --------------------------- cos(theta) & phi(theta) ---------------------------
         cosine = F.linear(F.normalize(input), F.normalize(self.weight))
-        sine = torch.sqrt(1.0 - torch.pow(cosine, 2))
+
+        # fix nan problem:
+        sine = torch.sqrt(torch.clamp((1.0 - torch.pow(cosine, 2)),1e-9,1))
+
         phi = cosine * self.cos_m - sine * self.sin_m
+        
+        # print(cosine)
+        # phi=torch.cos(torch.acos(cosine)+self.m)
         if self.easy_margin:
             phi = torch.where(cosine > 0, phi, cosine)
         else:
@@ -110,7 +110,7 @@ class ArcMarginProduct(nn.Module):
         output = (one_hot * phi) + ((1.0 - one_hot) * cosine)
         output *= self.s
 
-        return output   
+        return output
     
     
     
@@ -134,15 +134,13 @@ class CNN(nn.Module):
             nn.Conv2d(64,16,3, 2, 1),
             nn.ReLU())
         self.linear_layer = nn.Linear(16*4*4,3)
-        self.output_layer = nn.Linear(3,10,bias=False)
+        self.fc=nn.Linear(3,10)
     def forward(self, xs):
-        feat = self.hidden_layer(xs)
-        # print(feature.shape)
-        fc = feat.reshape(-1,16*4*4)
-        # print(fc.data.size())
-        feature = self.linear_layer(fc)
-        output = self.output_layer(feature)
-        return feature, output
+        feat = self.hidden_layer(xs) 
+        feature = feat.reshape(-1,16*4*4)
+        feature = self.linear_layer(feature)
+        out=self.fc(feature)
+        return feature,out
 def decet(feature,targets,epoch):
     color = ["red", "black", "yellow", "green", "pink",
     "gray", "lightgreen", "orange", "blue", "teal"]
@@ -185,8 +183,8 @@ CUDA = torch.cuda.is_available()
 if CUDA:
     net = net.cuda()    
       
-arcloss = ArcLoss(10, 3).cuda()
-larcloss = ArcMarginProduct(10, 3).cuda()
+arcloss = ArcLoss(3,10).cuda()
+pro_arcloss = ArcMarginProduct(3, 10).cuda()
 
 
 nllloss= nn.NLLLoss(reduction="sum").cuda()
@@ -194,13 +192,18 @@ focalloss=FocalLoss(gamma=0)
 
 
 optmizer =  torch.optim.SGD(net.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005)
-optmizerarc =  torch.optim.Adam(arcloss.parameters())
-
+if theta_clip:
+    optmizerarc =  torch.optim.Adam(pro_arcloss.parameters())
+else:
+    optmizerarc =  torch.optim.Adam(arcloss.parameters())
 
 save_pic_path=""
+
+best_acc=0
+
 if __name__ == '__main__':
 
-    num_epochs = 10
+    num_epochs = 20
     # net = net.to(device)
     for epoch in range(num_epochs):
         train_loss = []
@@ -216,37 +219,55 @@ if __name__ == '__main__':
         target = []
         for i, (x, y) in enumerate(train_loader):
 
-            if CUDA:
-                x = x.cuda()
-                y = y.cuda()
-            xs, ys = net(x) 
+          
+            x = x.cuda()
+            y = y.cuda()
+            xs,ys = net(x) 
             
+            if no_plus:
+                loss=nn.CrossEntropyLoss()(ys,y)
+            else:
+            
+            
+                if theta_clip:
+                    arc_loss= pro_arcloss(xs,y)
+                    
+                    if theta_clip_and_focalloss:
+                        loss = focalloss(arc_loss, y)
+                    else:
+                        loss = nn.CrossEntropyLoss()(arc_loss, y)
+    
+                else:
+                    arc_loss =  torch.log(arcloss(xs))
+                    arcface_loss = nllloss(arc_loss, y)
+                    loss = arcface_loss
+                    
+                
+            
+            iter_loss = loss.item() 
+
             ############ ACCURACY CAL. ###########
-            value =  torch.argmax(ys, dim=1)
+            if no_plus:
+                   value =  torch.argmax(ys, dim=1)
+            else:
+                   if theta_clip:
+                       arc_loss= pro_arcloss(xs,y)
+                   else:
+                       arc_loss =  torch.log(arcloss(xs))
+                   value =  torch.argmax(arc_loss, dim=1)
             acc =  torch.sum((value == y).float()) / len(y)
-            
-            # ################ LOSS CAL. ################
-            arc_loss =  torch.log(arcloss(xs))
-            focal_loss = focalloss(ys, y)
-            
-            
-            
-            arcface_loss = nllloss(arc_loss, y)
-            
-            
-            larcface_loss=nn.CrossEntropyLoss()(ys,y)
-            loss = larcface_loss+focal_loss
-            
-            # loss=arcface_loss
-            iter_loss = loss.item()
-            ###########################################
-            
             ############ BACK PROP #################
-            optmizer.zero_grad()
-            optmizerarc.zero_grad()
-            loss.backward()
-            optmizer.step()
-            optmizerarc.step()
+            if no_plus:
+                optmizer.zero_grad()
+                loss.backward()
+                optmizerarc.step()
+            else:
+                optmizer.zero_grad()
+                optmizerarc.zero_grad()
+                loss.backward()
+                optmizer.step()
+                optmizerarc.step()
+                
             ########################################
             feat.append(xs) 
             target.append(y)
@@ -266,13 +287,23 @@ if __name__ == '__main__':
         with torch.no_grad():
             for i, (x, y) in enumerate(test_loader):
 
-                if CUDA:
-                    x = x.cuda()
-                    y = y.cuda()
-                xs, ys = net(x) 
+             
+                x = x.cuda()
+                y = y.cuda()
+                xs,ys = net(x) 
                 
+                # ################ ARCFACE PRODUCT LOSS CAL. ################
+              
+
                 ############ ACCURACY CAL. ###########
-                value =  torch.argmax(ys, dim=1)
+                if no_plus:
+                    value =  torch.argmax(ys, dim=1)
+                else:
+                    if theta_clip:
+                        arc_loss= pro_arcloss(xs,y)
+                    else:
+                        arc_loss =  torch.log(arcloss(xs))
+                    value =  torch.argmax(arc_loss, dim=1)
                 acc =  torch.sum((value == y).float()) / len(y)
                 
   
@@ -280,7 +311,8 @@ if __name__ == '__main__':
                 test_accuracy.append(acc)
             print ('Epoch {}/{},test Accuracy: {:.3f}'
                 .format(epoch+1, num_epochs, sum(test_accuracy)/len(test_accuracy)) )
-    
+        if best_acc <sum(test_accuracy)/len(test_accuracy):
+            best_acc=sum(test_accuracy)/len(test_accuracy)
     
     
     
@@ -310,7 +342,6 @@ if __name__ == '__main__':
         embs = torch.cat(emb).cpu().numpy()
         y = torch.cat(y).cpu().numpy()
     import pandas as pd
-    import numpy as np
     import plotly.express as px
     
     tsne_df = pd.DataFrame(
@@ -324,8 +355,10 @@ if __name__ == '__main__':
     fig.show()
             
     
-    
-    
+print('best_acc:{}'.format(best_acc))    
+print('no_plus :{}'.format(no_plus))
+print('theta_clip :{}'.format(theta_clip))
+print('theta_clip_and_focalloss :{}'.format(theta_clip_and_focalloss))
     
     
     
